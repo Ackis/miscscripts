0dbcb33bcbf72a49d6ec4a510ec5ef4c904131ae
John Pasula <ackis81@gmail.com>
2017-11-29 09:23:53 -0700

    Make the DNS script pull from domains file instead of being hard coded into the script itself.

--- dns/duckdns.sh
+++ dns/duckdns.sh
@@ -2,6 +2,15 @@
 
 MY_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [ ! -f "${MY_PATH}/domains" ]; then
+	DOMAINS=(
+		"ackis"
+)
+	else
+	readarray -t DOMAINS < "${MY_PATH}/domains"
+fi
+
+
 if [ ! -f "${MY_PATH}/token" ]; then
         echo "No DuckDNS Token."
 	exit 1
@@ -22,14 +31,7 @@ exec > >(tee "$LOGFILE") 2>&1
 
 logger -p syslog.info "Updating Duck DNS"
 
-echo url="https://www.duckdns.org/update?domains=ackis&token=$TOKEN&ip=" | curl -k -o "${LOGLOC}/ackis.duckdns.org.log" -K -
-echo url="https://www.duckdns.org/update?domains=anonamouseackis&token=$TOKEN&ip=" | curl -k -o "$LOGLOC/anonamouseackis.duckdns.org.log" -K -
-echo url="https://www.duckdns.org/update?domains=btnackis&token=$TOKEN&ip=" | curl -k -o "$LOGLOC/btnackis.duckdns.org.log" -K -
-echo url="https://www.duckdns.org/update?domains=efnetackis&token=$TOKEN&ip=" | curl -k -o "$LOGLOC/efnetackis.duckdns.org.log" -K -
-echo url="https://www.duckdns.org/update?domains=freenodeackis&token=$TOKEN&ip=" | curl -k -o "$LOGLOC/freenodeackis.duckdns.org.log" -K -
-echo url="https://www.duckdns.org/update?domains=ggnackis&token=$TOKEN&ip=" | curl -k -o "$LOGLOC/ggnackis.duckdns.org.log" -K -
-echo url="https://www.duckdns.org/update?domains=ptpackis&token=$TOKEN&ip=" | curl -k -o "$LOGLOC/ptpackis.duckdns.org.log" -K -
-echo url="https://www.duckdns.org/update?domains=quakenetackis&token=$TOKEN&ip=" | curl -k -o "$LOGLOC/quakenetackis.duckdns.org.log" -K -
-echo url="https://www.duckdns.org/update?domains=snoonetackis&token=$TOKEN&ip=" | curl -k -o "$LOGLOC/snoonetackis.duckdns.org.log" -K -
-echo url="https://www.duckdns.org/update?domains=whatackis&token=$TOKEN&ip=" | curl -k -o "$LOGLOC/whatackis.duckdns.org.log" -K -
-
+for index in "${!DOMAINS[@]}"; do
+	echo "${DOMAINS[index]}"
+	echo url="https://www.duckdns.org/update?domains=${DOMAINS[index]}&token=$TOKEN&ip=" | curl -k -o "${LOGLOC}/${DOMAINS[index]}.duckdns.org.log" -K -
+done

3d2de89038c34f735a9e2bb42e875b1539cddc4e
John Pasula <ackis81@gmail.com>
2017-11-29 09:14:49 -0700

    Fix logging issue. Derp.

--- dns/duckdns.sh
+++ dns/duckdns.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Current Duck DNS Domains (8/10):
-#	ackis
-#	anonamouseackis
-#	efnetackis
-#	freenodeackis
-#	ptpackis
-#	quackenetackis
-#	snoonetackis
-#	whatackis
-
 MY_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ ! -f "${MY_PATH}/token" ]; then
@@ -21,21 +11,18 @@ else
 	echo "Token found: ${TOKEN}"
 fi
 
-LOGLOC="/opt/scripts/dns/log"
-LOGFILE="/opt/scripts/dns/log/duckdns.log"
+LOGLOC="/var/log/dns"
+LOGFILE="${LOGLOC}/duckdns.log"
 
 echo "Script output will be logged to $LOGFILE"
 
-#exec > "$LOGFILE" 2>&1
-#exec > >(tee -a "$LOGFILE") 2>&1
 exec > >(tee "$LOGFILE") 2>&1
 
-/opt/scripts/scriptheader.sh "Duck DNS Update"
+/opt/scripts/misc/scriptheader.sh "Duck DNS Update"
 
 logger -p syslog.info "Updating Duck DNS"
 
-echo url="https://www.duckdns.org/update?domains=ackis&token=$TOKEN&ip=" | curl -k -o "$LOGLOC/ackis.duckdns.org.log" -K -
-echo $?
+echo url="https://www.duckdns.org/update?domains=ackis&token=$TOKEN&ip=" | curl -k -o "${LOGLOC}/ackis.duckdns.org.log" -K -
 echo url="https://www.duckdns.org/update?domains=anonamouseackis&token=$TOKEN&ip=" | curl -k -o "$LOGLOC/anonamouseackis.duckdns.org.log" -K -
 echo url="https://www.duckdns.org/update?domains=btnackis&token=$TOKEN&ip=" | curl -k -o "$LOGLOC/btnackis.duckdns.org.log" -K -
 echo url="https://www.duckdns.org/update?domains=efnetackis&token=$TOKEN&ip=" | curl -k -o "$LOGLOC/efnetackis.duckdns.org.log" -K -

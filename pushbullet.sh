ed5c4f13265d8ee1dbf6f4f09c1d932d0543515d
John Pasula <ackis81@gmail.com>
2018-11-28 14:52:35 -0700

    Read the API_KEY from a file.

--- pushbullet.sh
+++ pushbullet.sh
@@ -3,7 +3,7 @@
 
 # These are the only variables you should need to change to customize your script
 INTROMESSAGE="TBD"
-CONFIG_DIR="/home/TESTHOOK/.config/pushbullet"
+CONFIG_DIR="/home/jpasula/.config/pushbullet"
 CONFIG_FILE="${CONFIG_DIR}/config"
 SCRIPT_NAME="Pushbullet"
 
@@ -14,9 +14,11 @@ LOG=true
 # Internal script variables
 SCRIPT_NAME="$0"
 MY_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+API_KEY=""
 
 function display_help() {
 	echo "Help WIP"
+	echo "-l|--log:		Enabled logging"
 	echo "-v|--verbose:		Verbose mode"
 	echo "-h|--help:		Display help"
 }
@@ -45,33 +47,25 @@ function print_and_log() {
 	fi
 }
 
-echo "${CONFIG_FILE}"
-
-# Check the config directory
-if [ -f "${CONFIG_FILE}" ]; then
-	print_and_log "${SCRIPT_NAME}: Config file found at ${CONFIG_FILE}." "debug"
-	API_KEY=$(head -n 1 "${CONFIG_FILE}")
-echo "${API_KEY}"
-# No crypt key file found, therefore assume that we can't run the server and exit.
-else
-	print_and_log "${SCRIPT_NAME}: No config file found." "debug"
-	exit 1
-fi
-
-echo "3"
-
 # Command line parameters:
+#	-l|--log:		Logging on
 #	-v|--verbose:		Verbose mode
 #	-h|--help:		Display help
 
 while [[ $# -gt 0 ]]
 do
-	key="$1"
+    key="$1"
+# TODO: Make verbose mode go first
 	case $key in
+	    	-l | --log )
+		    LOG=true
+		    print_and_log "${SCRIPT_NAME}: Logging enabled." "debug"
+		    shift
+		    ;;
 		-v | --verbose )
 			VERBOSE=true
+			print_and_log "${SCRIPT_NAME}: Verbose mode enabled." "debug"
 			shift
-			echo "Verbose mode enabled."
 			;;
 		-h | --help )
 			display_help
@@ -82,5 +76,26 @@ do
 			shift
 	esac
 done
+
+# Check the config directory
+if [ -f "${CONFIG_FILE}" ]; then
+    print_and_log "${SCRIPT_NAME}: Config file found at ${CONFIG_FILE}." "debug"
+    declare -A VARS
+    IFS="="
+        while read -r lvalue rvalue; do
+             VARS[$lvalue]=${rvalue}
+        done < "${CONFIG_FILE}"
+
+	if [ -z "${VARS[API_KEY]}" ]; then
+	    echo "damn"
+	    exit 1
+	else
+	    API_KEY="${VARS[API_KEY]}"
+	 fi
+# No config file, fail script
+else
+	print_and_log "${SCRIPT_NAME}: No config file found." "debug"
+	exit 1
+fi
 
 print_and_log "${INTROMESSAGE}"

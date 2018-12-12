7855c37aac3a2965614f625d5789ac4ef9bf9045
John Pasula <ackis81@gmail.com>
2018-12-12 15:14:44 -0700

    Few minor changes - look to the ASF config directory first for the cryptkey, and by default log to syslog.info.

--- startasf.sh
+++ startasf.sh
@@ -2,7 +2,7 @@
 
 # These are the only variables you should need to change to customize your script
 INTROMESSAGE="Starting Archi Steam Farm. Enjoy your badges."
-ASF_CONFIG_DIR="/opt/ArchiSteamFarm/config/"
+ASF_CONFIG_DIR="/opt/ArchiSteamFarm/config"
 
 # We want to run this as the ASF user
 # Why doesn't this work?
@@ -17,7 +17,7 @@ SCRIPT_NAME="$0"
 MY_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function display_help() {
-	echo "Help WIP"
+	echo "This script allows you to start ArchiSteamFarm with ease."
 	echo "-v|--verbose:		Verbose mode"
 	echo "-h|--help:		Display help"
 	echo "-s|--server:		Run in server mode"
@@ -43,22 +43,22 @@ function print_and_log() {
 		if [ ! -z "${2}" ] ; then
 			logger -t ArchiSteamFarm -p "syslog.${2}" "${1}"
 		else
-			logger -t ArchiSteamFarm -p syslog.debug "$1"
+			logger -t ArchiSteamFarm -p syslog.info "$1"
 		fi
 	fi
 }
 
-# Check the path where we ran the script for our crypt key
-if [ -f "${MY_PATH}/cryptkey" ]; then
-	print_and_log "${SCRIPT_NAME}: cryptkey found at ${MY_PATH}/cryptkey." "debug"
-	CRYPTKEY=$(head -n 1 "${MY_PATH}/cryptkey")
 # Check the ASF config directory for the crypt key
-elif [ -f "${ASF_CONFIG_DIR}/cryptkey" ]; then
-	print_and_log "${SCRIPT_NAME}: cryptkey file found at ${ASF_CONFIG_DIR}/cryptkey." "debug"
+if [ -f "${ASF_CONFIG_DIR}/cryptkey" ]; then
+	print_and_log "Cryptkey file found at ${ASF_CONFIG_DIR}/cryptkey."
 	CRYPTKEY=$(head -n 1 "${ASF_CONFIG_DIR}/cryptkey")
+# Check the path where we ran the script for our crypt key
+elif [ -f "${MY_PATH}/cryptkey" ]; then
+	print_and_log "Cryptkey found at ${MY_PATH}/cryptkey."
+	CRYPTKEY=$(head -n 1 "${MY_PATH}/cryptkey")
 # No crypt key file found, therefore assume that we can't run the server and exit.
 else
-	print_and_log "${SCRIPT_NAME}: No cryptkey found." "debug"
+	print_and_log "No cryptkey found."
 	exit 1
 fi
 
@@ -93,8 +93,8 @@ print_and_log "${INTROMESSAGE}"
 
 if [ "${SERVERMODE}" = true ] ; then
 	print_and_log "Starting ASF in server mode."
-	/opt/ArchiSteamFarm/ArchiSteamFarm --cryptkey="${CRYPTKEY}" --server
+#	/opt/ArchiSteamFarm/ArchiSteamFarm --cryptkey="${CRYPTKEY}" --server
 else
 	print_and_log "Starting ASF in normal mode."
-	/opt/ArchiSteamFarm/ArchiSteamFarm --cryptkey="${CRYPTKEY}"
+#	/opt/ArchiSteamFarm/ArchiSteamFarm --cryptkey="${CRYPTKEY}"
 fi

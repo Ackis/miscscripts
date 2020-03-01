ec751661e90a8e887dbfcaae7dcf4b1b8fda4a51
John Pasula <ackis81@gmail.com>
2020-02-29 22:02:38 -0700

    Finish script, it'll now update the cloudflare IP address.

--- dns/cloudflare.sh
+++ dns/cloudflare.sh
@@ -3,6 +3,7 @@
 # These are the only variables you should need to change to customize your script
 INTROMESSAGE="Updating dynamic DNS service CloudFlare."
 CONFIG_DIR="/opt/scripts/misc/dns/"
+AUTH_EMAIL="cjpasula@gmail.com"
 
 # Display extra logging info
 VERBOSE=true
@@ -11,12 +12,12 @@ LOG=true
 SCRIPT_NAME="$0"
 MY_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ ! -f "${MY_PATH}/domains" ]; then
+if [ ! -f "${MY_PATH}/cloudflare_domains" ]; then
 	DOMAINS=(
 		"ackis"
 )
 	else
-	readarray -t DOMAINS < "${MY_PATH}/domains"
+	readarray -t DOMAINS < "${MY_PATH}/cloudflare_domains"
 fi
 
 function print_and_log() {
@@ -36,21 +37,21 @@ function print_and_log() {
 	if [ "${LOG}" = true ] ; then
 		# Were we passed a second argmument?
 		if [ ! -z "${2}" ] ; then
-			logger -t DuckDNS -p "syslog.${2}" "${1}"
+			logger -t CloudflareDNS -p "syslog.${2}" "${1}"
 		else
-			logger -t DuckDNS -p syslog.debug "$1"
+			logger -t CloudflareDNS -p syslog.debug "$1"
 		fi
 	fi
 }
 
 # Check the path where we ran the script for our key
-if [ -f "${MY_PATH}/token" ]; then
-	print_and_log "Token found at ${MY_PATH}/token." "info"
-	TOKEN=$(head -n 1 "${MY_PATH}/token")
+if [ -f "${MY_PATH}/cloudflare_token" ]; then
+	print_and_log "Token found at ${MY_PATH}/cloudflare_token." "info"
+	AUTH_KEY=$(head -n 1 "${MY_PATH}/cloudflare_token")
 # Check the config directory for the key
-elif [ -f "${CONFIG_DIR}/token" ]; then
-	print_and_log "Token file found at ${CONFIG_DIR}/token." "info"
-	TOKEN=$(head -n 1 "${CONFIG_DIR}/token")
+elif [ -f "${CONFIG_DIR}/cloudflare_token" ]; then
+	print_and_log "Token file found at ${CONFIG_DIR}/cloudflare_token." "info"
+	AUTH_KEY=$(head -n 1 "${CONFIG_DIR}/cloudflare_token")
 # No crypt key file found, therefore assume that we can't run and exit.
 else
 	print_and_log "No token found." "error"
@@ -59,29 +60,30 @@ fi
 
 print_and_log "${INTROMESSAGE}" "info"
 
+# Used code from https://gist.github.com/benkulbertis/fff10759c2391b6618dd
 for index in "${!DOMAINS[@]}"; do
-	print_and_log "Updating dynamic IP of ${DOMAINS[index]}.duckdns.org" "info"
+	print_and_log "Updating dynamic IP of ${DOMAINS[index]}" "info"
+
+	IP=$(curl -s http://ipv4.icanhazip.com)
+	ZONE_IDENTIFIER=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAINS[index]}" -H "X-Auth-Email: ${AUTH_EMAIL}" -H "X-Auth-Key: ${AUTH_KEY}" -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
+	RECORD_IDENTIFIER=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_IDENTIFIER/dns_records?name=${DOMAINS[index]}" -H "X-Auth-Email: ${AUTH_EMAIL}" -H "X-Auth-Key: ${AUTH_KEY}" -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*')
 
 	# Construct the URL
-	# Leaving the IP4 blank, the service detects our IP.
-	# Get the IP6 address of the system
-	IPV6=$(ip -6 -o addr show up primary scope global | while read -r num dev fam addr rest; do echo ${addr%/*}; done)
-
-	if [ -z "${IPV6}" ] ; then
-		echo "Error: No IP6 address found."
-		URL="https://www.duckdns.org/update?domains=${DOMAINS[index]}&token=${TOKEN}&verbose=${VERBOSE}"
-	else
-		URL="https://www.duckdns.org/update?domains=${DOMAINS[index]}&token=${TOKEN}&ip=&ipv6=${IPV6}&verbose=${VERBOSE}"
-	fi
+	URL="\"https://api.cloudflare.com/client/v4/zones/${ZONE_IDENTIFIER}/dns_records/${RECORD_IDENTIFIER}\" -H \"X-Auth-Email: ${AUTH_EMAIL}\" -H \"X-Auth-Key: ${AUTH_KEY}\" -H \"Content-Type: application/json\" --data '{\"type\":\"A\",\"name\":\""${DOMAINS[index]}"\",\"content\":\""${IP}"\",\"ttl\":120,\"proxied\":false}'"
 
 	echo "Sending URL: ${URL}"
-	CMD=$(curl -s -S -f "${URL}")
+	CMD=$(curl -s -X PUT "${URL}")
 	STATUS="$?"
 	echo "CMD ${CMD}"
 	echo "STATUS ${STATUS}"
-	if [ "${STATUS}" = 0 ] ; then
-		print_and_log "Successfully updated dynamic IP of ${DOMAINS[index]}.duckdns.org" "info"
-	else
-		print_and_log "Failed opdating dynamic IP of ${DOMAINS[index]}.duckdns.org" "error"
-	fi
+
+	case "$CMD" in
+		*"\"success\":false"*)
+			message="API UPDATE FAILED. DUMPING RESULTS:\n$update"
+			echo -e "$message"
+			exit 1;;
+		*)
+			message="IP changed to: $IP"
+			echo "$message";;
+	esac
 done

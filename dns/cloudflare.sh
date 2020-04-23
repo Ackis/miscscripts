01c5a04a0ee5c6ede93b4e2881f1cbb74efbcc0c
John Pasula <ackis81@gmail.com>
2020-04-23 14:41:31 -0600

    Move to new common functions.

--- dns/cloudflare.sh
+++ dns/cloudflare.sh
@@ -20,49 +20,27 @@ if [ ! -f "${MY_PATH}/cloudflare_domains" ]; then
 	readarray -t DOMAINS < "${MY_PATH}/cloudflare_domains"
 fi
 
-function print_and_log() {
-	# If we weren't passed an argument, fail.
-	if [ -z "${1}" ] ; then
-		echo "Error: No parameter passed to print_and_log. Function needs at least one string."
-		return 1
-	fi
-
-	# If verbose is enabled, spout more text.
-	if [ "${VERBOSE}" = true ] ; then
-		# We can assume we have an argument here
-		echo "${1}"
-	fi
-
-	# Do we want to log?
-	if [ "${LOG}" = true ] ; then
-		# Were we passed a second argmument?
-		if [ ! -z "${2}" ] ; then
-			logger -t CloudflareDNS -p "syslog.${2}" "${1}"
-		else
-			logger -t CloudflareDNS -p syslog.debug "$1"
-		fi
-	fi
-}
+source /opt/scripts/misc/common.sh
 
 # Check the path where we ran the script for our key
 if [ -f "${MY_PATH}/cloudflare_token" ]; then
-	print_and_log "Token found at ${MY_PATH}/cloudflare_token." "info"
+	print_and_log "Token found at ${MY_PATH}/cloudflare_token." "CloudflareDNS" "info"
 	AUTH_KEY=$(head -n 1 "${MY_PATH}/cloudflare_token")
 # Check the config directory for the key
 elif [ -f "${CONFIG_DIR}/cloudflare_token" ]; then
-	print_and_log "Token file found at ${CONFIG_DIR}/cloudflare_token." "info"
+	print_and_log "Token file found at ${CONFIG_DIR}/cloudflare_token." "CloudflareDNS" "info"
 	AUTH_KEY=$(head -n 1 "${CONFIG_DIR}/cloudflare_token")
 # No crypt key file found, therefore assume that we can't run and exit.
 else
-	print_and_log "No token found." "error"
+	print_and_log "No token found." "CloudflareDNS" "error"
 	exit 1
 fi
 
-print_and_log "${INTROMESSAGE}" "info"
+print_and_log "${INTROMESSAGE}" "CloudflareDNS" "info"
 
 # Used code from https://gist.github.com/benkulbertis/fff10759c2391b6618dd
 for index in "${!DOMAINS[@]}"; do
-	print_and_log "Updating dynamic IP of ${DOMAINS[index]}" "info"
+	print_and_log "Updating dynamic IP of ${DOMAINS[index]}" "CloudflareDNS" "info"
 
 	IP=$(curl -s http://ipv4.icanhazip.com)
 	ZONE_IDENTIFIER=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAINS[index]}" -H "X-Auth-Email: ${AUTH_EMAIL}" -H "X-Auth-Key: ${AUTH_KEY}" -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
@@ -79,11 +57,11 @@ for index in "${!DOMAINS[@]}"; do
 
 	case "$CMD" in
 		*"\"success\":false"*)
-			message="API UPDATE FAILED. DUMPING RESULTS:\n$update"
+			message="API UPDATE FAILED. DUMPING RESULTS:\n$CMD"
 			echo -e "$message"
 			exit 1;;
 		*)
-			message="IP changed to: $IP"
+			message="IP changed to: $IP\n$CMD"
 			echo "$message";;
 	esac
 done
